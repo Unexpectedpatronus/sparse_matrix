67a34ef9997c148c328d01c5a1f2b77e8902fa7a
Evgeny Odintsov <ev1genial@gmail.com>
2024-01-05 23:55:28 +0300

    Add dynamic allocation of memory

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <iomanip>
-
+#include <fstream>
 #include "list.hpp"
 
 #define R 5
@@ -8,7 +8,7 @@
 #define K_1 3  // koef of sparsity
 #define K_2 2  // koef of sparsity
 
-void compress(int matrix[][C], List *AN, List *D) {
+void compress(int **matrix, List *AN, List *D) {
     AN->add(0);
     AN->cur->num = matrix[0][0];
     D->add(0);
@@ -27,7 +27,7 @@ void compress(int matrix[][C], List *AN, List *D) {
     }
 }
 
-void unpack(int matrix[][C], List *AN, List *D) {
+void unpack(int **matrix, List *AN, List *D) {
     AN->cur = AN->first;
     D->cur = D->first;
     matrix[0][0] = AN->cur->num;
@@ -48,12 +48,12 @@ void unpack(int matrix[][C], List *AN, List *D) {
     }
 }
 
-void printMatrix(int matrix[][C], const std::string &name) {
+void printMatrix(int **matrix, const std::string &name) {
     std::cout << "Matrix " << name << ":\n";
     for (int i = 0; i < R; i++) {
         for (int j = 0; j < C; j++) {
-            if (j <= i)
-                std::cout << std::setw(2) << matrix[i][j] << " ";
+//            if (j <= i)
+            std::cout << std::setw(2) << matrix[i][j] << " ";
         }
         std::cout << std::endl;
     }
@@ -124,7 +124,21 @@ void sumMatrices(List *A_AN, List *A_D, List *B_AN, List *B_D, List *S_AN, List 
 }
 
 int main() {
-    int A[R][C], res_A[R][C], B[R][C], res_B[R][C], S[R][C], res_S[R][C];
+    int **A = new int *[R];
+    int **res_A = new int *[R];
+    int **B = new int *[R];
+    int **res_B = new int *[R];
+    int **S = new int *[R];
+    int **res_S = new int *[R];
+    for (int i = 0; i < R; ++i) {
+        A[i] = new int[C];
+        res_A[i] = new int[C];
+        B[i] = new int[C];
+        res_B[i] = new int[C];
+        S[i] = new int[C];
+        res_S[i] = new int[C];
+    }
+
     List *A_AN, *A_D, *B_AN, *B_D, *S_AN, *S_D;
     A_AN = new List();
     A_D = new List();
@@ -133,20 +147,34 @@ int main() {
     S_AN = new List();
     S_D = new List();
 
-    for (int i = 0; i < R; i++) {
-        for (int j = 0; j < C; j++) {
-            if ((i + j) % K_1 == 0)
-                A[i][j] = i * j + i + j;
-            else A[i][j] = 0;
+//    for (int i = 0; i < R; i++) {
+//        for (int j = 0; j < C; j++) {
+//            if ((i + j) % K_1 == 0)
+//                A[i][j] = i * j + i + j;
+//            else A[i][j] = 0;
+//        }
+//    }
+//    for (int i = 0; i < R; i++) {
+//        for (int j = 0; j < C; j++) {
+//            if ((i + j) % K_2 == 0)
+//                B[i][j] = i * j + i + j;
+//            else B[i][j] = 0;
+//        }
+//    }
+
+    std::ifstream inA("A.txt");
+    std::ifstream inB("B.txt");
+    if (inA.is_open() && inB.is_open()) {
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < R; j++) {
+                inA >> A[i][j];
+                inB >> B[i][j];
+            }
         }
     }
-    for (int i = 0; i < R; i++) {
-        for (int j = 0; j < C; j++) {
-            if ((i + j) % K_2 == 0)
-                B[i][j] = i * j + i + j;
-            else B[i][j] = 0;
-        }
-    }
+    inA.close();
+    inB.close();
+
 
     printMatrix(A, "A");
     compress(A, A_AN, A_D);
@@ -167,12 +195,53 @@ int main() {
             S[i][j] = A[i][j] + B[i][j];
         }
     }
-    printMatrix(S, "S");
+    printMatrix(S, "S = A + B");
 
     sumMatrices(A_AN, A_D, B_AN, B_D, S_AN, S_D);
     unpack(res_S, S_AN, S_D);
-    printMatrix(res_S, "res_S");
+    printMatrix(res_S, "unpacked_S = A + B");
 
+    int unmatch = 0;
+    for (int i = 0; i < R; i++) {
+        if (unmatch)
+            break;
+        for (int j = 0; j < C; j++) {
+            if (S[i][j] != res_S[i][j]) {
+                unmatch++;
+                break;
+            }
+        }
+    }
+    if (unmatch)
+        std::cout << "Error." << std::endl;
+    else {
+        std::cout << "Matrices are equal." << std::endl;
+        std::ofstream outS("S.txt");
+        if (outS.is_open()) {
+            for (int i = 0; i < R; i++) {
+                for (int j = 0; j < R; j++) {
+                    outS << std::setw(2) << res_S[i][j] << " ";
+                }
+                outS << std::endl;
+            }
+        }
+        outS.close();
+    }
+
+    for (int i = 0; i < R; ++i) {
+        delete[] A[i];
+        delete[] res_A[i];
+        delete[] B[i];
+        delete[] res_B[i];
+        delete[] S[i];
+        delete[] res_S[i];
+    }
+    delete[] A;
+    delete[] res_A;
+    delete[] B;
+    delete[] res_B;
+    delete[] S;
+    delete[] res_S;
     delete A_AN;
     delete A_D;
     delete B_AN;
